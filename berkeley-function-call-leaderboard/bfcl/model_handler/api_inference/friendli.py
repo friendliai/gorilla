15fb3d009ddbca8f3eead8cbe0b64f8c4e7fdbbe
minpeter <kali2005611@gmail.com>
2025-02-25 22:50:15 +0900

    Add new model `meta-llama-3.3-70b-instruct` to FSE_MODELS and clean up code formatting

--- berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/friendli.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/friendli.py
@@ -8,12 +8,13 @@ class FriendliHandler(OpenAIHandler):
     FSE_MODELS = [
         "meta-llama-3.1-8b-instruct",
         "meta-llama-3.1-70b-instruct",
+        "meta-llama-3.3-70b-instruct",
         # Add additional model names for fse here.
     ]
 
     def __init__(self, model_name, temperature) -> None:
         """
-        e.g. model_name = 
+        e.g. model_name =
             - "meta-llama-3.1-8b-instruct" (fse) -> "https://api.friendli.ai/serverless/v1"
             - "450abXXVVXX" (fde) -> "https://api.friendli.ai/dedicated/v1"
             - "450abXXVVXX:tdlr" (fde lora adapter) -> "https://api.friendli.ai/dedicated/v1"
@@ -25,8 +26,7 @@ class FriendliHandler(OpenAIHandler):
         model_id, base_url = self.route_model(model_name)
 
         super().__init__(model_id, temperature)
-        
-        
+
         self.client = OpenAI(
             base_url=base_url,
             api_key=os.getenv("FRIENDLI_TOKEN"),
@@ -45,16 +45,18 @@ class FriendliHandler(OpenAIHandler):
     def route_model(model_name: str):
         """
         Determine the base_url based on the model name.
-        
+
         Args:
             model_name (str): The model identifier.
-        
+
         Returns:
             tuple: (model_name, base_url)
         """
         if model_name in FriendliHandler.FSE_MODELS:  # Friendli Serverless Endpoints
             base_url = "https://api.friendli.ai/serverless/v1"
-        elif model_name.isdigit():  # Friendli Container - model name is a numeric string
+        elif (
+            model_name.isdigit()
+        ):  # Friendli Container - model name is a numeric string
             base_url = f"http://localhost:{model_name}/v1"
         elif model_name.startswith("http://"):  # Friendli Container - full URL
             base_url = f"{model_name}/v1"
